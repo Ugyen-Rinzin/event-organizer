89b9127999a686d8da568e2a684e1602702a3311
Ugyen-Rinzin <ugyen.namgongpa@gmail.com>
2024-08-17 14:54:31 +0600

    6 commit

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@ boxmenu.addEventListener("click", () => {
 function hit() {
   alert("Thank you for contacting us!!!");
 }
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
+// toggleButton.addEventListener("click", () => {
+//   navbarLinks.classList.toggle("active");
+// });
